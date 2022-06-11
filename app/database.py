e0097a4abfff0ee2e8c9165183a7b0bac117a260
Alex Kosh <sasha23969@gmail.com>
2022-06-11 03:40:20 +0300

    feat: Added get_user_by_token and update_user_token

--- app/database.py
+++ app/database.py
@@ -46,3 +46,16 @@ def get_user_by_username(db: Session, username: str) -> Optional[User]:
     """Return User from db by unique username."""
     statement = select(User).where(User.username == username)
     return db.exec(statement).first()
+
+
+def get_user_by_token(db: Session, token: str) -> Optional[User]:
+    """Return User from db by unique token."""
+    statement = select(User).where(User.token == token)
+    return db.exec(statement).first()
+
+
+def update_user_token(db: Session, user: User, new_token: str):
+    user.token = new_token
+    db.add(user)
+    db.commit()
+    db.refresh(user)
