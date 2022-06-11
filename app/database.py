689c9c4cb36c5df3def89d8e2ce88f0ba73d6354
stepf <stepfedorov@yandex.ru>
2022-06-11 15:57:44 +0300

    fix: Now token is uniq

--- app/database.py
+++ app/database.py
@@ -34,7 +34,10 @@ def get_db() -> Session:
 class User(SQLModel, table=True):
     """Class to represent bookmarks owners."""
 
-    __table_args__ = (UniqueConstraint('username'),)
+    __table_args__ = (
+        UniqueConstraint('username'),
+        UniqueConstraint('token'),
+    )
 
     id: Optional[int] = Field(default=None, primary_key=True)
     username: str
