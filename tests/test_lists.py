c0a17e5de325f0d504641b3de9dbdd020ff5baa3
stepf <stepfedorov@yandex.ru>
2022-06-19 02:29:01 +0300

    test: удален лишний тест

--- tests/test_lists.py
+++ tests/test_lists.py
@@ -1,12 +1,13 @@
 import sys
+
 import pytest
 
 sys.path.append('.')
 sys.path.append('..')
 
 from app.database import get_test_db
-from app.lists.crud import (get_user_lists, get_list_by_id,
-                            create_list, update_list_by_id, delete_list_by_id)
+from app.lists.crud import (create_list, delete_list_by_id, get_list_by_id,
+                            get_user_lists, update_list_by_id)
 from app.lists.models import BookmarkList
 from app.users.models import User
 
@@ -21,23 +22,13 @@ def test_create_list():
         assert (get_list_by_id(db, 1) == test_list)
 
 
-def test_get_list():
-    for db in get_test_db():
-        assert(get_list_by_id(db, 1) is None)
-        
-        test_list = BookmarkList(name='List1')
-        db.add(test_list)
-        db.commit()
-
-        assert (get_list_by_id(db, 1) == test_list)
-
-
 def test_update_list():
     for db in get_test_db():
         test_list = BookmarkList(name='List1')
         db.add(test_list)
         db.commit()
 
+        assert (get_list_by_id(db, 1).name == 'List1')
         update_list_by_id(db, 1, 'NewName')
         
         assert (get_list_by_id(db, 1).name == 'NewName')
@@ -48,6 +39,8 @@ def test_delete_list():
         test_list = BookmarkList(name='List1')
         db.add(test_list)
         db.commit()
+
+        assert (get_list_by_id(db, 1) != None)
 
         delete_list_by_id(db, 1)
 
